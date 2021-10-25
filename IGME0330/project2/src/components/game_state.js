bb8087b2e5df6b1dff18737e082b2c8d0f4aef37
Matheus Laurentys <mtlaurentys@gmail.com>
2021-10-25 14:11:27 -0400

    refactoring main app

--- IGME0330/project2/src/components/game_state.js
+++ IGME0330/project2/src/components/game_state.js
@@ -1,14 +1,7 @@
 //import cytoscape from 'https://cdn.jsdelivr.net/npm/cytoscape@3.19.1/dist/cytoscape.esm.min.js';
 import { API_DATA } from "../env.js";
 import { API_ENDPOINT, IMAGES_ENDPOINT } from "../consts.js";
-import {
-  loadMovieById,
-  loadPeopleOptions,
-  loadMovieOptions,
-  loadPersonByName,
-  loadPersonById,
-  loadRandomPerson,
-} from "../utils.js";
+import { loadPersonByName, loadRandomPerson } from "../utils.js";
 
 const template = document.createElement("template");
 template.innerHTML = `
@@ -49,6 +42,16 @@ template.innerHTML = `
 </div>
 `;
 
+function dispatch(element, eventName, detail) {
+  element.dispatchEvent(
+    new CustomEvent(eventName, {
+      composed: true,
+      bubbles: true,
+      detail: detail,
+    })
+  );
+}
+
 class MDBCGameState extends HTMLElement {
   constructor() {
     super();
@@ -57,107 +60,87 @@ class MDBCGameState extends HTMLElement {
     this.sourceCard = this.shadowRoot.querySelector("#source-card");
     this.currentCard = this.shadowRoot.querySelector("#current-card");
     this.targetCard = this.shadowRoot.querySelector("#target-card");
-    this.btPrev = this.shadowRoot.querySelector("#prevBt");
-    this.btNext = this.shadowRoot.querySelector("#nextBt");
-    this.shadowRoot.querySelector("#change-source").onclick = async () => {
-      this.startObj = await loadRandomPerson();
-      this.selectedObj = this.startObj;
-      this.page = 0;
-      this.optionsList = await loadMovieOptions(this.startObj.id);
-      this.render();
-    };
-    this.shadowRoot.querySelector("#change-target").onclick = async () => {
-      this.targetObj = await loadRandomPerson();
-      this.renderStatus();
-      if (this.targetObj.id === this.selectedObj.id) this.winGame();
-    };
-    this.shadowRoot.querySelector("#search-source").onclick = async () => {
-      this.startObj = await loadPersonByName(
-        this.shadowRoot.querySelector("#text-source").value
-      );
-      this.selectedObj = this.startObj;
-      this.page = 0;
-      this.optionsList = await loadMovieOptions(this.startObj.id);
-      this.render();
-    };
-    this.shadowRoot.querySelector("#search-target").onclick = async () => {
-      this.targetObj = await loadPersonByName(
-        this.shadowRoot.querySelector("#text-target").value
-      );
-      if (this.targetObj.id === this.selectedObj.id) this.winGame();
-      this.renderStatus();
-    };
-    this.shadowRoot.querySelector("#new-game").onclick = () => this.loadAsync();
-    this.optionsElement = this.shadowRoot.querySelector("mdbc-options");
-    this.shadowRoot.addEventListener(
-      "optionSelected",
-      this.handleOptionSelected
-    );
     this.startObj = null;
     this.targetObj = null;
     this.selectedObj = null;
-    this.optionsList = null;
     this.currentPath = [];
     this.browsing = "movies";
     this.page = 0;
-    this.btPrev.onclick = () => {
-      if (this.page > 0) {
-        this.page--;
-        this.renderOptions();
-      }
-    };
-    this.btNext.onclick = () => {
-      this.page++;
-      this.renderOptions();
-    };
     this.loadAsync = this.loadAsync.bind(this);
-    this.renderStatus = this.renderStatus.bind(this);
-    this.renderOptions = this.renderOptions.bind(this);
+    this.attachCallbacks = this.attachCallbacks.bind(this);
+    this.attachCallbacks();
     this.loadAsync();
+  }
+
+  attachCallbacks() {
+    this.shadowRoot.querySelector("#change-source").onclick = dispatch(
+      this,
+      "selectedRandomSource",
+      this.shadowRoot.querySelector("#text-source").value
+    );
+    this.shadowRoot.querySelector("#change-target").onclick = dispatch(
+      this,
+      "selectedRandomTarget"
+    );
+    this.shadowRoot.querySelector("#search-source").onclick = dispatch(
+      this,
+      "selectedSource",
+      this.parsedData[detail].id
+    );
+    this.shadowRoot.querySelector("#search-target").onclick = dispatch(
+      this,
+      "selectedSource",
+      this.shadowRoot.querySelector("#text-target").value
+    );
+    this.shadowRoot.querySelector("#new-game").onclick = () => {
+      dispatch(this, "selectedNewGame");
+    };
   }
 
   async loadAsync() {
     this.targetObj = await loadRandomPerson();
     this.startObj = await loadRandomPerson();
     this.selectedObj = this.startObj;
-    this.optionsList = await loadMovieOptions(this.startObj.id);
     this.render();
   }
 
-  winGame = () => {
-    alert("Congratulations!");
-    const previousRuns = localStorage.getItem("movies_cartographer") || "[]";
-    let runs = JSON.parse(previousRuns);
-    runs.push(this.currentPath);
-    localStorage.setItem("movies_cartographer", JSON.stringify(runs));
-  };
+  disconnectedCallback() {}
 
-  handleOptionSelected = async ({ detail }) => {
-    this.page = 0;
-    if (detail === this.targetObj.id) {
-      this.winGame();
-      return;
-    }
-    let newObj, newList;
-    if (this.browsing === "movies") {
-      newObj = await loadMovieById(detail);
-      newList = await loadPeopleOptions(newObj.id);
-    } else {
-      newObj = await loadPersonById(detail);
-      newList = await loadMovieOptions(newObj.id);
-    }
-    if (newObj.name && newList.length > 0) {
-      this.optionsList = newList;
-      this.selectedObj = newObj;
-      if (this.browsing === "movies") this.browsing = "people";
-      else this.browsing = "movies";
-    } else {
-      alert("Search unsuccesful. Select another");
+  attributeChangedCallback(name) {
+    switch (name) {
+      case "source":
+        this.sourceObj = {};
+        break;
+
+      default:
+        break;
     }
     this.render();
-  };
+  }
 
-  renderStatus() {
+  static get observedAttributes() {
+    return ["source", "current", "target"];
+  }
+
+  renderSource() {
+    this.sourceCard.dataset.label = `Start: ${this.startObj.name}`;
+    this.sourceCard.dataset.alt = `Photo of ${this.startObj.name}`;
+    this.sourceCard.dataset.source = `${IMAGES_ENDPOINT}${this.startObj.img_path}`;
+  }
+
+  renderCurrent() {
+    this.currentCard.dataset.label = `Current: ${this.selectedObj.name}`;
+    this.currentCard.dataset.alt = `Photo of ${this.selectedObj.name}`;
+    this.currentCard.dataset.source = `${IMAGES_ENDPOINT}${this.selectedObj.img_path}`;
+  }
+
+  renderTarget() {
+    this.targetCard.dataset.label = `Target: ${this.targetObj.name}`;
+    this.targetCard.dataset.alt = `Photo of ${this.targetObj.name}`;
+    this.targetCard.dataset.source = `${IMAGES_ENDPOINT}${this.targetObj.img_path}`;
+  }
+
+  render() {
     //note it alternates Person -> Movie -> Person
     if (
       this.currentPath.length === 0 ||
@@ -166,25 +149,9 @@ class MDBCGameState extends HTMLElement {
       this.currentPath.push(this.selectedObj.id);
     }
     if (!this.startObj || !this.selectedObj || !this.targetObj) return;
-    this.sourceCard.dataset.label = `Start: ${this.startObj.name}`;
-    this.sourceCard.dataset.alt = `Photo of ${this.startObj.name}`;
-    this.sourceCard.dataset.source = `${IMAGES_ENDPOINT}${this.startObj.img_path}`;
-    this.currentCard.dataset.label = `Current: ${this.selectedObj.name}`;
-    this.currentCard.dataset.alt = `Photo of ${this.selectedObj.name}`;
-    this.currentCard.dataset.source = `${IMAGES_ENDPOINT}${this.selectedObj.img_path}`;
-    this.targetCard.dataset.label = `Target: ${this.targetObj.name}`;
-    this.targetCard.dataset.alt = `Photo of ${this.targetObj.name}`;
-    this.targetCard.dataset.source = `${IMAGES_ENDPOINT}${this.targetObj.img_path}`;
-  }
-
-  renderOptions() {
-    this.optionsElement.dataset.options = JSON.stringify(this.optionsList);
-    this.optionsElement.dataset.index = this.page;
-  }
-
-  render() {
-    this.renderStatus();
-    this.renderOptions();
+    this.renderSource();
+    this.renderCurrent();
+    this.renderTarget();
   }
 }
 
